52712f237bc31fe7f8b1cf66d6bd835db8f62dc1
Deniulor <417928448@qq.com>
2017-06-03 12:02:37 +0800

    同步到公司

--- d-client/src/dragon/model/battle/Config.ts
+++ d-client/src/dragon/model/battle/Config.ts
@@ -2,6 +2,19 @@ module dragon.battle0 {
 	class Config {
 		public CriticalMultiple: number = 1.2;
 
+
+		private attrFuncs: { [k: number]: Function };
+		public attrFun(attr: enums.Attribute) {
+			if (!this.attrFuncs) {
+				let all = kernel.data.group('attrdefine').all();
+				
+			}
+			return this.attrFuncs[attr];
+		}
+
+		private defaultAttrFunc(u: Unit, attr: enums.Attribute) {
+			return u.baseAttr(attr);
+		}
 	}
 
 	export const config = new Config();
